e824fd73461c0807fc3050528dae39e2ab1590e0
vudangkhoa <dangkhoa11227@gmail.com>
2022-04-20 12:36:33 +0700

    fix #1

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require('cors');
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
 
 //IMPORT ROUTES
-const userRoutes = require('./routes/userRouters');
-const notificationRoutes = require('./routes/notificationRouters');
-const payrollRoutes = require('./routes/payrollRouters');
-const dayoffRoutes = require('./routes/dayoffRouters');
+const userRoutes = require("./routes/userRouters");
+const notificationRoutes = require("./routes/notificationRouters");
+const payrollRoutes = require("./routes/payrollRouters");
+const dayoffRoutes = require("./routes/dayoffRouters");
 
-app.use('/uploads', express.static('uploads'));
-app.use('/api/users', userRoutes);
-app.use('/api/notifications', notificationRoutes);
-app.use('/api/payrolls', payrollRoutes);
-app.use('/api/dayoffs', dayoffRoutes);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/users", userRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/payrolls", payrollRoutes);
+app.use("/api/dayoffs", dayoffRoutes);
 
 //GET
-app.get('/', (req, res) => {
-  res.send('API is running...');
-})
+app.get("/", (req, res) => {
+	res.send("API is running...");
+});
 
 app.use(notFound);
 app.use(errorHandler);
 
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, () =>
-  console.log('CONNECT TO DATABASE!')
+	console.log("CONNECT TO DATABASE!")
 );
 
 //create new table in mongoose
